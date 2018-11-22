aba49298ad5cc3ea8956cfcd509adb3672f01109
optikfluffel <optik@fluffel.io>
2018-11-22 17:28:16 +0100

    move shortcuts into src

---
+++ src/shortcuts.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    emoji: "📚",
+    name: "Wiki",
+    description: "35c3 Wiki",
+    href: "https://events.ccc.de/congress/2018"
+  },
+  {
+    emoji: "💬",
+    name: "IRC Chat",
+    description: "Web client for #35c3 channel on irc.hackint.org",
+    href: "https://webirc.hackint.org/#irc://irc.hackint.org/#35c3"
+  },
+  {
+    emoji: "🚃",
+    name: "LVB Connection Info",
+    description: "Fahrplanauskunft - Leipziger Verkehrsbetriebe",
+    href: "https://www.l.de/verkehrsbetriebe/fahrplan"
+  }
+]
