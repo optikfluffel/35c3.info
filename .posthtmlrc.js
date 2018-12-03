8e29f87f7a2b8b8d5f3a171b69f07c9d40a9a61b
optikfluffel <optik@fluffel.io>
2018-12-03 14:26:45 +0100

    move shortcuts into .posthtmlc.js

--- .posthtmlrc.js
+++ .posthtmlrc.js
@@ -1,6 +1,34 @@
+const shortcutList = [
+  // TODO: Add again, after it's open for the public.
+  //
+  // {
+  //   emoji: "📚",
+  //   name: "Wiki",
+  //   description: "35C3 Wiki",
+  //   href: "https://events.ccc.de/congress/2018"
+  // },
+  {
+    emoji: "☑️",
+    name: "Congress Checklist",
+    description: "MacLemon/CongressChecklist at Github",
+    href: "https://github.com/MacLemon/CongressChecklist#congresschecklist"
+  },
+  {
+    emoji: "💬",
+    name: "IRC Chat",
+    description: "Web client for #35c3 channel on irc.hackint.org",
+    href: "https://webirc.hackint.org/#irc://irc.hackint.org/#35c3"
+  },
+  {
+    emoji: "🚃",
+    name: "LVB Connection Info",
+    description: "Fahrplanauskunft - Leipziger Verkehrsbetriebe",
+    href: "https://www.l.de/verkehrsbetriebe/fahrplan"
+  }]
+
 const locals = {
   title: '35C3 Refreshing Memories',
-  shortcutList: require('./src/shortcuts')
+  shortcutList: shortcutList
 }
 
 module.exports = {
